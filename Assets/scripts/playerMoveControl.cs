e7c1fc9d7da119a2eeff53dc3b6e3725c47615f2
Hector-bit <hectormartinez.2978@gmail.com>
2020-10-17 23:16:34 -0700

    character movement + jump functionality

---
+++ Assets/scripts/playerMoveControl.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerMoveControl : MonoBehaviour
+{
+    public myCharCont controller;
+
+    public float runSpeed = 40f;
+
+    float horizontalMove = 0f;
+    bool jump = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+        if(Input.GetButtonDown("Jump"))
+        {
+            jump = true;
+        }
+    }
+
+    void FixedUpdate ()
+    {
+        //Move our character
+        controller.Move(horizontalMove * Time.fixedDeltaTime, false, jump);
+        jump = false;
+    }
+}
