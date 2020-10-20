2f4f91c93f2f66bd0345a9e7c0f7299d1e9daff0
Hector-bit <hectormartinez.2978@gmail.com>
2020-10-20 12:50:08 -0700

    edited ropeanchor script to incorporate sprites for grabbling hook

---
+++ Assets/scripts/ropeSystem.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ropeSystem : MonoBehaviour {
+    // 1
+    public GameObject ropeHingeAnchor;
+    public DistanceJoint2D ropeJoint;
+    public Transform crosshair;
+    public SpriteRenderer crosshairSprite;
+    public playerMoveControl playerMovement;
+    private bool ropeAttached;
+    private Vector2 playerPosition;
+    private Rigidbody2D ropeHingeAnchorRb;
+    private SpriteRenderer ropeHingeAnchorSprite;
+
+    //2nd set of variables from that one tutorial
+    public LineRenderer ropeRenderer;
+    public LayerMask ropeLayerMask;
+    private float ropeMaxCastDistance = 20f;
+    private List<Vector2> ropePositions = new List<Vector2>();
+
+    void Awake()
+    {
+        // 2
+        ropeJoint.enabled = false;
+        playerPosition = transform.position;
+        ropeHingeAnchorRb = ropeHingeAnchor.GetComponent<Rigidbody2D>();
+        ropeHingeAnchorSprite = ropeHingeAnchor.GetComponent<SpriteRenderer>();
+    }
+
+    void Update()
+    {
+        // 3
+        var worldMousePosition =
+            Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 0f));
+        var facingDirection = worldMousePosition - transform.position;
+        var aimAngle = Mathf.Atan2(facingDirection.y, facingDirection.x);
+        if (aimAngle < 0f)
+        {
+            aimAngle = Mathf.PI * 2 + aimAngle;
+        }
+
+        // 4
+        var aimDirection = Quaternion.Euler(0, 0, aimAngle * Mathf.Rad2Deg) * Vector2.right;
+        // 5
+        playerPosition = transform.position;
+
+        // 6
+        if (!ropeAttached)
+        {
+            SetCrosshairPosition(aimAngle);
+        }
+        else
+        {
+            crosshairSprite.enabled = false;
+        }
+    }
+};
