a6f6e4b2119dac0a33ff03778122a7eb7b5a6577
Hector-bit <hectormartinez.2978@gmail.com>
2020-12-11 01:59:20 -0800

    level select design

--- Assets/scripts/levelSelect.cs
+++ Assets/scripts/levelSelect.cs
@@ -1,44 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class levelSelect : MonoBehaviour
 {
-    public GameObject levelHolder;
-    public GameObject levelIcon;
-    public GameObject thisCanvas;
-    //remember to change number of levels as you create levels
-    public int numberOfLevels = 3;
-
-    // Start is called before the first frame update
-    void Start()
+    [SerializedField] private bool unlocked; 
+    public Image unlockImage; //find a way to use a dimmed version of the planet instead of an image
+    // public GameObject[] stars;
+    
+    private void UpdateLevelImage()
     {
-        Rect panelDimensions = levelHolder.GetComponent<RectTransform().rect;
-        Rect iconDimensions = levelIcon.GetComponent<RectTransform().rect;
-        int maxInARow = Mathf.FloorToInt(panelDimensions.width / iconDimensions.width);
-        int maxInACol = Mathf.FloorToInt(panelDimensions.height / iconDimensions.height);
-        int amountPerPage = maxInARow * maxInACol;
-        int totalPages = Mathf.CeilToInt((float)numberOfLevels / amountPerPage);
-        LoadPanels(totalPages);
-    }
-
-    void LoadPanels(int numberOfPanels){
-        Debug.Log(numberOfPanels);
-        GameObject panelClone = Instantiate)levelHolder) as GameObject;
-        for(int i = ; i <= numberOfPanels; i++){
-            GameObject panel = Instantiate(panelClone) as GameObject;
-            panel.transform.SetParent(levelHolder.transform);
-            panel.name = "Page-" + i;
-            panel.GetComponent<RectTransform>().localPosition = new vector2(panelDimensions.width * (i-1), 0);
-            LoadIcons(25, panel);
-        }
-    }
-
-    void LoadIcons(int numberOfIcons, GameObject parentObject){
-        for(int i = 1; i <= numberOfIcons; i++){
-            GameObject icon = Instant(levelIcon) as GameObject;
-            icon.transform.SetParent(thisCanvas.transform, false);
-            icon.name = "Level " + i;
+        if(!unlocked){
+            unlockImage.gameObject.SetActive(true);
+        } else {
+            unlockImage.gameObject.SetActive(false);
         }
     }
 }
