d849808cf6126c0b8c5654a6131376123ac4f785
Hector-bit <hectormartinez.2978@gmail.com>
2020-12-12 17:56:30 -0800

    planet level select for moon complete

---
+++ Assets/scripts/LevelSelection.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class LevelSelection : MonoBehaviour
+{
+    [SerializeField] private bool unlocked; 
+    public Image unlockImage; //find a way to use a dimmed version of the planet instead of an image
+    // public GameObject[] stars;
+
+    private void Start(){
+        UpdateLevelImage();
+    }
+    
+    private void UpdateLevelImage()
+    {
+        if(!unlocked){
+            unlockImage.gameObject.SetActive(true);
+        } else {
+            unlockImage.gameObject.SetActive(false);
+        }
+    }
+
+    public void PressSelection(string levelName){
+        if(unlocked){
+            SceneManager.LoadScene(levelName);
+        }
+    }
+}
