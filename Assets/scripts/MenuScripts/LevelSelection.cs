362955e5d5c0962694e39a011c9593a59c98d34b
Hector-bit <hectormartinez.2978@gmail.com>
2020-12-19 13:23:54 -0800

    planets will now unlock through a badge system that stays with the player

--- Assets/scripts/MenuScripts/LevelSelection.cs
+++ Assets/scripts/MenuScripts/LevelSelection.cs
@@ -4,28 +4,45 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
-public class LevelSelection : MonoBehaviour
+// This Script will be responsible for level selection and keeping track
+// of which levels are unlocked
+
+public class LevelSelection : stats
 {
-    [SerializeField] private bool unlocked; 
-    public Image unlockImage; //find a way to use a dimmed version of the planet instead of an image
+    [SerializeField] private bool moonUnlocked = false;
+    [SerializeField] private bool marsUnlocked = false; 
+    public Image moonUnlockImage; //find a way to use a dimmed version of the planet instead of an image
     // public GameObject[] stars;
+    // public string[] planetsUnlocked = new string[] {"moonBadge", "marsBadge"};
 
     private void Start(){
-        UpdateLevelImage();
+        // Debug.Log("HELLO" + planetBadges);
+        foreach (string badge in planetBadges)
+        {
+            Debug.Log(badge);
+            if(badge == "moonBadge"){moonUnlocked = true;}
+            if(badge == "marsBadge"){marsUnlocked = true;}
+        }
+
+        UpdateLevelImage(moonUnlocked);
     }
     
-    private void UpdateLevelImage()
+    private void UpdateLevelImage(bool moon)
     {
-        if(!unlocked){
-            unlockImage.gameObject.SetActive(true);
+        if(moon == true){
+            moonUnlockImage.gameObject.SetActive(false);
         } else {
-            unlockImage.gameObject.SetActive(false);
+            moonUnlockImage.gameObject.SetActive(false);
         }
+        
+        // if(mars == true){
+        //     unlockImage.gameObject.SetActive(true);
+        // } else {
+        //     unlockImage.gameObject.SetActive(false);
+        // }
     }
 
     public void PressSelection(string levelName){
-        if(unlocked){
-            SceneManager.LoadScene(levelName);
-        }
+        SceneManager.LoadScene(levelName);
     }
 }
