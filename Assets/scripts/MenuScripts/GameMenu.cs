3fc4f8b3e9d77fbf2deb9d5bf45fe55bb26be70b
Hector-bit <hectormartinez.2978@gmail.com>
2020-12-25 11:35:32 -0800

    In game menu so the player can exit the game

---
+++ Assets/scripts/MenuScripts/GameMenu.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameMenu : MonoBehaviour
+{
+    [SerializeField] private bool Gamemenu;
+
+    private void start(){
+        GameMenu = false;
+    }
+
+
+    //This function will be added to a button that will open and close the menu
+    private void menuSelection(){
+        if(GameMenu == false){
+            GameMenu = true;
+        } else {
+            GameMenu = false;
+        }
+    }
+}
