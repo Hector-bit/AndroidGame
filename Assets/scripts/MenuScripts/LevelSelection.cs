93cc9d2d40871859d70a1225b76678a3e606f15f
Hector-bit <hectormartinez.2978@gmail.com>
2020-12-25 23:40:47 -0800

    redid level select section because it was deleted

--- Assets/scripts/MenuScripts/LevelSelection.cs
+++ Assets/scripts/MenuScripts/LevelSelection.cs
@@ -7,41 +7,8 @@ using UnityEngine.SceneManagement;
 // This Script will be responsible for level selection and keeping track
 // of which levels are unlocked
 
-public class LevelSelection : stats
+public class LevelSelection : MonoBehaviour
 {
-    [SerializeField] private bool moonUnlocked = false;
-    [SerializeField] private bool marsUnlocked = false; 
-    public Image moonUnlockImage; //find a way to use a dimmed version of the planet instead of an image
-    // public GameObject[] stars;
-    // public string[] planetsUnlocked = new string[] {"moonBadge", "marsBadge"};
-
-    private void Start(){
-        // Debug.Log("HELLO" + planetBadges);
-        foreach (string badge in planetBadges)
-        {
-            Debug.Log(badge);
-            if(badge == "moonBadge"){moonUnlocked = true;}
-            if(badge == "marsBadge"){marsUnlocked = true;}
-        }
-
-        UpdateLevelImage(moonUnlocked);
-    }
-    
-    private void UpdateLevelImage(bool moon)
-    {
-        if(moon == true){
-            moonUnlockImage.gameObject.SetActive(false);
-        } else {
-            moonUnlockImage.gameObject.SetActive(false);
-        }
-        
-        // if(mars == true){
-        //     unlockImage.gameObject.SetActive(true);
-        // } else {
-        //     unlockImage.gameObject.SetActive(false);
-        // }
-    }
-
     public void PressSelection(string levelName){
         SceneManager.LoadScene(levelName);
     }
