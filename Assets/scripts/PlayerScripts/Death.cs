aad1309580dc35db8aa13196c9fc9e37249df10a
Hector-bit <hectormartinez.2978@gmail.com>
2021-01-02 07:18:33 -0800

    respawning happens when you git a certain collision

---
+++ Assets/scripts/PlayerScripts/Death.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Player dies then respawns with this script
+
+public class Death : MonoBehaviour
+{
+   public void OnCollisionEnter2D(Collision2D collision) {
+        if (collision.gameObject.CompareTag("Enemy")) {
+            Destroy(gameObject);
+            stats.instance.Respawn();
+        }
+    }
+}
