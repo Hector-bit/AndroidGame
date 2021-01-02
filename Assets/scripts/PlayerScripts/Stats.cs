8eacc84c8e1ba24d30ea56ce944180695ce969a7
Hector-bit <hectormartinez.2978@gmail.com>
2021-01-02 14:31:41 -0800

    new type or respawn method

--- Assets/scripts/PlayerScripts/Stats.cs
+++ Assets/scripts/PlayerScripts/Stats.cs
@@ -5,19 +5,23 @@ using Cinemachine;
 
 public class Stats : MonoBehaviour
 {
-    public static Stats instance;
+    //Everything that is commented out was used for a different type of respawn
+    //method that I tried and may or may not try to use again.
+
+    
+    // public static Stats instance;
     public string[] planetBadges = new string[] {"moonBadge", "marsBadge", "planetOneBadge"};
-    public Transform respawnPoint;
-    public GameObject playerPrefab;
+    // public Transform respawnPoint;
+    // public GameObject playerPrefab;
 
-    public CinemachineVirtualCameraBase cam;
+    // public CinemachineVirtualCameraBase cam;
 
-    private void Awake() {
-        instance = this;
-    }
+    // private void Awake() {
+    //     instance = this;
+    // }
 
-    public void Respawn () {
-        GameObject player = Instantiate(playerPrefab, respawnPoint.position, Quaternion.identity);
-        cam.Follow = player.transform;
-    }
+    // public void Respawn () {
+    //     GameObject player = Instantiate(playerPrefab, respawnPoint.position, Quaternion.identity);
+    //     cam.Follow = player.transform;
+    // }
 }
