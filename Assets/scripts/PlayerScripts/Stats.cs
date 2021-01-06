faeb97cc01c09b0e391ef893c0db762ee794c5cd
Hector-bit <hectormartinez.2978@gmail.com>
2021-01-06 03:36:08 -0800

    spaceship trimmed and updated victory script

--- Assets/scripts/PlayerScripts/Stats.cs
+++ Assets/scripts/PlayerScripts/Stats.cs
@@ -3,6 +3,10 @@ using System.Collections.Generic;
 using UnityEngine;
 using Cinemachine;
 
+//Level1 is the mars level
+//Level2 is the planet1 llevel
+//Level3 is the moon level
+
 public class Stats : MonoBehaviour
 {
     //Everything that is commented out was used for a different type of respawn
--- Assets/scripts/enviromentScripts/Victory.cs
+++ Assets/scripts/enviromentScripts/Victory.cs
@@ -3,17 +3,34 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class Victory : MonoBehaviour
+//Level1 is the mars level
+//Level2 is the planet1 llevel
+//Level3 is the moon level
+
+public class Victory : Stats
 {
-    // Start is called before the first frame update
+    // public playerBadgeCase = Stats.planetBadges;
+    private string[] planetBadges = {"marsBadge", "planetOneBadge", "moonBadge"};
+    private string[] levelNames = {"Level1", "Level2", "Level3"};
+    private string badgeToBeEarned = null;
+
     void Start()
     {
-        Scene currentScene = SceneManager.GetActiveScene();
+        Scene currentScene = SceneManager.GetActiveScene(); 
+        for(int i = 0; i < levelNames.Length; i ++){
+            if(levelNames[i] == currentScene.name){
+                badgeToBeEarned = planetBadges[i];
+            }
+            // Debug.Log("We are on level: " + levelNames[i] + " and this is the badge " + badgeToBeEarned);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    void ReachedTheSpaceship()
     {
-        
+        //once the player reaches the spaceship a couple things will happen
+        //first they will earn a badge for the current level
+        Stats.planetBadges.add(badgeToBeEarned);
+        //second a victory canvas will appear
+
     }
-}
+} 
